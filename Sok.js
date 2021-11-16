e2370353122a32232eecb479ff8f0f7ff473e075
Sokheng <jiangshuxing009@gmail.com>
2021-11-16 10:39:52 -0800

    changes

--- Sok.js
+++ Sok.js
@@ -1,3 +1,28 @@
-console.log('test1')
-console.log('test2')
-console.log('test3')
+// console.log('test1')
+// console.log('test2')
+// console.log('test3')
+
+const API = 'https://api.dictionaryapi.dev/api/v2/entries/en/hello'
+
+// const API1 = 'https://api.dictionaryapi.dev/api/v2/entries/en/<word>'
+fetch(API)
+.then(res => res.json())
+.then(word => {
+    document.getElementById('definition').innerText = word.word
+})
+
+document
+.getElementById('search-form')
+.addEventListener('submit', addWord)
+
+document
+.getElementById('search-button')
+.addEventListener('submit', button)
+
+function button(event){
+    event.preventDefault()
+}
+
+function addWord(event){
+    event.preventDefault()
+}
