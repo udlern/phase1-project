b6b1ed4bc6e9f2836c928097165562a950f6c3b6
Noa Udler <udlernoa4@gmail.com>
2021-11-16 10:35:51 -0800

    trying to get it work

--- Noa.js
+++ Noa.js
@@ -1,2 +1,13 @@
-console.log('hello!')
+const searchButton = document.querySelector('#search-button')
+searchButton.addEventListener('click', handleSearchButton) 
+
+function handleSearchButton (event) {
+    event.preventDefault()
+const searchInput = document.querySelector('.search-input')
+const searchInputValue = searchInput.value
+const API = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchInputValue}`
+fetch(API)
+.then(resp => resp.json())
+.then(data => console.log(data))
+}
 
